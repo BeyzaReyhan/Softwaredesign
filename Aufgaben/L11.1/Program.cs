550d73235917682d63643f2b3345c2788505df4b
Beyza Reyhan Altintas <beyza.reyhan.altintas@hs-furtwangen.de>
2019-01-13 17:12:40 +0100

    Add files via upload

---
+++ Aufgaben/L11.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L11._1
+
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.CalculateSomething();
+        }
+    }
+}
