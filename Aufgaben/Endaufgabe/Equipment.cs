894b91674f9acea33a0f81e260b417aa300bb44a
Beyza Reyhan Altintas <beyza.reyhan.altintas@hs-furtwangen.de>
2019-02-11 06:12:47 +0100

    Delete Equipment.cs

--- Aufgaben/Endaufgabe/Equipment.cs
+++
@@ -1,7 +0,0 @@
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-//[JsonConverter(typeof(StringEnumConverter))]
-enum Equipment {Computer, Kreidetafel, Whiteboard, Ansteigend, Laborausstattung};
-    
